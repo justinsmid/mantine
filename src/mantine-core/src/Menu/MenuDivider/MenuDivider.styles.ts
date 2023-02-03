81211aea7787e3fe22904f6521063c4047e6c944
Justin Smid <justin.smid@getmarvia.com>
2023-02-03 17:09:23 +0100

    [@mantine/core] MenuDivider: Fix invalid `calc()`

--- src/mantine-core/src/Menu/MenuDivider/MenuDivider.styles.ts
+++ src/mantine-core/src/Menu/MenuDivider/MenuDivider.styles.ts
@@ -2,7 +2,7 @@ import { createStyles, rem } from '@mantine/styles';
 
 export default createStyles((theme) => ({
   divider: {
-    margin: `calc(${theme.spacing.xs} / 2) - ${rem(5)}`,
+    margin: `calc((${theme.spacing.xs} / 2) - ${rem(5)})`,
     borderTop: `${rem(1)} solid ${
       theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
     }`,
